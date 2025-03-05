386d1b6feff394ff653ab9f8a2295114042386ed
Bryan Miller <millerwbryan@gmail.com>
2025-03-04 21:52:40 -0300

    Time used by band in GppProgramProvider

--- scheduler/scripts/gpp_programs.py
+++ scheduler/scripts/gpp_programs.py
@@ -15,7 +15,7 @@ from pyexplore.pyexplore import explore, schema
 
 from scheduler.core.programprovider.gpp.gppprogramprovider import GppProgramProvider
 from scheduler.core.sources.sources import Sources
-from lucupy.minimodel.observation import ObservationClass
+from lucupy.minimodel.observation import ObservationClass, Band
 
 if __name__ == '__main__':
 
@@ -24,15 +24,16 @@ if __name__ == '__main__':
 
     # List programs
     # TODO change pyexplore to other api query
-    programs = explore.get_programs(include_deleted=False)
-    # programs = []
-    progid = None
-    for p in programs:
-        print(f'{p["id"]}: {p["name"]}')
-        # progid = p["id"] if progid is None else progid
-    print("")
+    # programs = explore.get_programs(include_deleted=False)
+    # # programs = []
+    # progid = None
+    # for p in programs:
+    #     print(f'{p["id"]}: {p["name"]}')
+    #     # progid = p["id"] if progid is None else progid
+    # print("")
 
-    progid = 'p-913'
+    # progid = 'p-913'
+    progid = 'p-139'
 
     # TODO change pyexplore to other api query
     prog = explore.get_program(progid)
@@ -49,9 +50,29 @@ if __name__ == '__main__':
     print(prog_mini.start, prog_mini.end)
     # print(prog_mini.allocated_time)
     print(prog_mini.program_awarded(), prog_mini.partner_awarded(), prog_mini.total_awarded())
-    print(prog_mini.used_time)
     print(prog_mini.bands())
+    print(f'total_awarded: {prog_mini.total_awarded()}')
+    print(f'total_awarded Band 1: {prog_mini.total_awarded(Band(1))}')
+    print(f'program_awarded Band 2: {prog_mini.program_awarded(Band(2))}')
+    print(f'partner_awarded Band 1: {prog_mini.partner_awarded(Band(1))}')
+    print('Allocated time')
     for alloc in prog_mini.allocated_time:
         print(alloc.category.name, alloc.program_awarded, alloc.band.name)
-
-
+    print('used_time')
+    for charge in prog_mini.used_time:
+        print(charge.band.name, charge.program_used, charge.partner_used, charge.not_charged)
+    print('Previouly used time methods')
+    print(f'\tprogram: {prog_mini.program_previously_used()}')
+    print(f'\tpartner: {prog_mini.partner_previously_used()}')
+    print(f'\ttotal: {prog_mini.total_previously_used()}')
+    print(f'\tprogram Band 1: {prog_mini.program_previously_used(Band(1))}')
+    print(f'\tpartner Band 1: {prog_mini.partner_previously_used(Band(1))}')
+    print(f'\ttotal Band 1: {prog_mini.total_previously_used(Band(1))}')
+    print('Total used time methods')
+    print(f'\tprogram: {prog_mini.program_used()}')
+    print(f'\tprogram Band 1: {prog_mini.program_used(Band(1))}')
+    print(f'\tpartner: {prog_mini.partner_used()}')
+    print(f'\tpartner Band 1: {prog_mini.partner_used(Band(1))}')
+    # print(f'\ttotal: {prog_mini.total_used()}')
+    print(f'\ttotal: {prog_mini.total_used()}')
+    print(f'\ttotal Band 1: {prog_mini.total_used(Band(1))}')
