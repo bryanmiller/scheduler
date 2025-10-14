2cbca1a85c720ffa77d65252293efe9db953c3fd
Bryan Miller <millerwbryan@gmail.com>
2025-10-14 17:51:28 -0300

    GSCHED-871 investigation
    # Conflicts:
    #	scheduler/pickles/ocsresource.pickle

---
+++ scheduler/scripts/run_gsched-871.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+# For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+import asyncio
+import os
+import time
+from pathlib import Path
+
+from astropy.time import Time
+from lucupy.minimodel.site import ALL_SITES, Site
+from lucupy.observatory.abstract import ObservatoryProperties
+from lucupy.observatory.gemini import GeminiProperties
+
+
+from definitions import ROOT_DIR
+from scheduler.core.builder.modes import SchedulerModes
+from scheduler.core.components.ranker import RankerParameters
+from scheduler.engine import SchedulerParameters, Engine
+from scheduler.services import logger_factory
+from scheduler.services.visibility import visibility_calculator
+from scheduler.core.statscalculator.plot_stats import plot_stats
+
+_logger = logger_factory.create_logger(__name__)
+
+def main(*,
+         programs_ids: Path = Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids.txt') -> None:
+
+    # Set lucupy to Gemini
+    ObservatoryProperties.set_properties(GeminiProperties)
+
+    # Grab visibility calculations from Reddit
+    asyncio.run(visibility_calculator.calculate())
+
+    # Parsed program file (this replaces the program picker from Schedule)
+    # with open(programs_ids, 'r') as file:
+    #     programs_list = [line.strip() for line in file if line.strip()[0] != '#']
+    programs_list = ['GS-2018B-Q-127', 'GS-2018B-Q-128', 'GS-2018B-Q-224']
+
+    # Create Parameters
+    # params = SchedulerParameters(start=Time("2018-08-02 08:00:00", format='iso', scale='utc'),
+    params = SchedulerParameters(start=Time("2018-12-11 08:00:00", format='iso', scale='utc'),
+    # params = SchedulerParameters(start=Time("2019-01-18 08:00:00", format='iso', scale='utc'),
+                                     end=Time("2019-01-31 08:00:00", format='iso', scale='utc'),
+                              # sites=ALL_SITES,
+                               sites=[Site.GS],
+                               mode=SchedulerModes.VALIDATION,
+                               ranker_parameters=RankerParameters(),
+                               # ranker_parameters=RankerParameters(vis_power=0.5, air_power=1.0),
+                               semester_visibility=False,
+                               num_nights_to_schedule=14,
+                               programs_list=programs_list)
+    engine = Engine(params)
+    plan_summary, timelines = engine.schedule()
+    # File output for future results comparison
+    outpath = os.path.join(os.environ['HOME'], 'gemini', 'sciops', 'softdevel', 'Queue_planning', 'sched_output')
+    # timelines.display(output=os.path.join(outpath, 'dev_niri_s20181001_20250715.txt'))
+    # timelines.display(output=os.path.join(outpath, 'gn_s20180802_180_vp0p5_ap1_20250826.txt'))
+    # timelines.display(output=os.path.join(outpath, 'gn_s20181001_3_20250911.txt'))
+    # Display to stdout
+    # timelines.display()
+    print(plan_summary)
+
+    # plot_stats(plan_summary, output=os.path.join(outpath, 'gn_s20180802_180_vp0p5_ap1_20250826.png'))
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    # main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids.redis.txt')
+    # main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids_gn.redis.txt')
+    main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids.txt')
+    print(f'Completed in {(time.time() - t0) / 60.} min')
