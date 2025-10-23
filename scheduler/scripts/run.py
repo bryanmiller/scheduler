b224468fbfe1c896307e93abfbfdfc0ba31232cc
Bryan Miller <millerwbryan@gmail.com>
2025-10-23 12:07:22 -0300

    Parallelizie visibility calculations
    # Conflicts:
    #	scheduler/core/components/collector/collector.py

--- scheduler/scripts/run.py
+++ scheduler/scripts/run.py
@@ -36,38 +36,39 @@ def main(*,
         programs_list = [line.strip() for line in file if line.strip()[0] != '#']
 
     # Create Parameters
-    params = SchedulerParameters(start=Time("2018-10-01 08:00:00", format='iso', scale='utc'),
-                                 end=Time("2018-10-05 08:00:00", format='iso', scale='utc'),
+    # params = SchedulerParameters(start=Time("2018-10-01 08:00:00", format='iso', scale='utc'),
+    #                              end=Time("2018-10-05 08:00:00", format='iso', scale='utc'),
     # params = SchedulerParameters(start=Time("2018-12-15 08:00:00", format='iso', scale='utc'),
     #                              end=Time("2018-12-20 08:00:00", format='iso', scale='utc'),
-    # params=SchedulerParameters(start=Time("2018-08-01 08:00:00", format='iso', scale='utc'),
-    #                            end=Time("2018-10-01 08:00:00", format='iso', scale='utc'),
+    params=SchedulerParameters(start=Time("2018-08-01 08:00:00", format='iso', scale='utc'),
+                               end=Time("2018-10-01 08:00:00", format='iso', scale='utc'),
     # params=SchedulerParameters(start=Time("2018-08-02 08:00:00", format='iso', scale='utc'),
     #                           end=Time("2019-01-31 08:00:00", format='iso', scale='utc'),
-                              sites=ALL_SITES,
-                              #  sites=[Site.GN],
+    #                           sites=ALL_SITES,
+                               sites=[Site.GN],
                                mode=SchedulerModes.VALIDATION,
                                ranker_parameters=RankerParameters(),
                                # ranker_parameters=RankerParameters(vis_power=0.5, air_power=1.0),
                                semester_visibility=False,
-                               num_nights_to_schedule=2,
+                               num_nights_to_schedule=1,
                                programs_list=programs_list)
     engine = Engine(params)
     plan_summary, timelines = engine.schedule()
     # File output for future results comparison
     outpath = os.path.join(os.environ['HOME'], 'gemini', 'sciops', 'softdevel', 'Queue_planning', 'sched_output')
-    # timelines.display(output=os.path.join(outpath, 'dev_niri_s20181001_20250715.txt'))
+    # timelines.display(output=os.path.join(outpath, 'dev_1m_s20180801_20251022_redis.txt'))
     # timelines.display(output=os.path.join(outpath, 'gn_s20180802_180_vp0p5_ap1_20250826.txt'))
     # timelines.display(output=os.path.join(outpath, 'gn_s20181001_3_20250911.txt'))
     # Display to stdout
     timelines.display()
+    print(plan_summary)
 
     # plot_stats(plan_summary, output=os.path.join(outpath, 'gn_s20180802_180_vp0p5_ap1_20250826.png'))
 
 
 if __name__ == '__main__':
     t0 = time.time()
-    # main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'progr am_ids.redis.txt')
-    # main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids_gn.redis.txt')
-    main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids.txt')
+    # main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids.redis.txt')
+    main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids_gn.redis.txt')
+    # main(programs_ids=Path(ROOT_DIR) / 'scheduler' / 'data' / 'program_ids.txt')
     print(f'Completed in {(time.time() - t0) / 60.} min')
