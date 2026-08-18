6f54a14c8863b5d4421e3db1702dc83ffc1f389e
Bryan Miller <millerwbryan@gmail.com>
2026-08-18 10:35:21 -0400

    Initial work on multiple rankers
    # Conflicts:
    #	backend/scheduler/engine/engine.py

--- backend/scheduler/scripts/run.py
+++ backend/scheduler/scripts/run.py
@@ -3,6 +3,7 @@
 
 import asyncio
 import os
+import time
 from pathlib import Path
 from datetime import datetime
 from zoneinfo import ZoneInfo
@@ -36,24 +37,30 @@ def main(*,
     # Create Parameters
     # params = SchedulerParameters(start=datetime.fromisoformat("2018-10-04 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
     #                              end=datetime.fromisoformat("2018-10-10 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
-    # params = SchedulerParameters(start=datetime.fromisoformat("2018-10-01 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
-    #                              end=datetime.fromisoformat("2018-10-03 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
-    params = SchedulerParameters(start=datetime.fromisoformat("2018-12-12 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
-                                 end=datetime.fromisoformat("2018-12-18 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
-                                 # sites=ALL_SITES,
+    params = SchedulerParameters(start=datetime.fromisoformat("2018-08-02 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
+                                 end=datetime.fromisoformat("2018-08-10 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
+    # params = SchedulerParameters(start=datetime.fromisoformat("2018-12-12 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
+    #                              end=datetime.fromisoformat("2018-12-18 08:00:00").replace(tzinfo=ZoneInfo("UTC")),
+    #                              sites=ALL_SITES,
                                  sites=[Site.GN],
                                  mode=SchedulerModes.VALIDATION,
                                  ranker_parameters=RankerParameters(),
-                                 semester_visibility=False,
-                                 num_nights_to_schedule=1,
+                                 semester_visibility=True,
+                                 num_nights_to_schedule=3,
                                  programs_list=programs_list,
                                  use_local_visibility=True)
     engine = Engine(params)
     plan_summary, timelines = engine.schedule()
+    # File output for future results comparison
+    # outpath = os.path.join(os.environ['HOME'], 'gemini', 'sciops', 'softdevel', 'Queue_planning', 'sched_output')
+    # timelines.display(output=os.path.join(outpath, 'gn_1min_s20180801_184_20260817.txt'))
+
     timelines.display()
     print(plan_summary)
 
 if __name__ == '__main__':
+    t0 = time.time()
     # programs_ids = Path(ROOT_DIR) / 'scheduler/data' / 'program_ids.txt'
     programs_ids = Path(ROOT_DIR) / 'scheduler/data' / 'program_ids_gn.redis.txt'
     main(programs_ids=programs_ids)
+    print(f'Completed in {(time.time() - t0) / 60.} min')
